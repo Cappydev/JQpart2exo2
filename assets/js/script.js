45c76e75bb8e15ddd9a8d1efa298e55b707d2fab
cappydev <kevin.lanel@wanadoo.fr>
2019-02-05 16:05:51 +0100

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
-
-$(document).ready(function(){
-    $('#image').dblclick(function() {
-      $('#image').css('width', '500px');
-
-    });
+$(function(){
+  var count = 0;
+  $('#buttonUp').click(function(){ //clique sur le bouton +
+    count++; //ajoute 1 au compteur
+    $('#number').val(count);
+  });
+  $('#buttonDown').click(function(){ //clique sur le bouton -
+    count--; // retire 1 au compteur
+    $('#number').val(count);
+  });
 });
